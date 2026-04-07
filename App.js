5194e98aa2d8f8d3ae589222618141523f43396b
Paulo Gabriel <gab.lyma7@gmail.com>
2026-04-07 18:48:06 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+        <Header />
+      <ScrollView>
+
+
       <StatusBar style="auto" />
-    </View>
+      </ScrollView>
+      </View>
   );
 }
 
---
+++ components/Header/index.js
@@ -0,0 +1,70 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default function Header() {
+    const day = new Date().getDate();
+    const month = new Date().getMonth() + 1;
+    const year = new Date().getFullYear();
+
+    return (
+        <View style={styles.container}>
+        <Text style={styles.title}>Gerenciador de atividades</Text>
+
+            <View style={styles.header}>
+            <Text style={styles.data}>{day}/{month}/{year}</Text>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {}}>
+                    <Text style={styles.plus}>+</Text>
+                </TouchableOpacity>
+                />
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        marginTop: 40,
+        backgroundColor: '#4ea1b9',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '90%',
+        borderRadius: 8,
+        height: 110,
+        top: 0,
+    },
+    header: {
+        flexDirection: 'row',
+    },
+    data: {
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius: 10,
+        width: '50%',
+        textAlign: 'center',
+        fontSize: 18
+    },
+    button: {
+    backgroundColor: 'white',
+        padding: 10,
+        borderRadius: '70%',
+        width: 50,
+        height: 50,
+        marginLeft: 10,
+
+    },
+    plus: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        fontSize: 25,
+
+    },
+    title: {
+        fontSize: 18,
+        color: 'white',
+        fontWeight: 'bold',
+        marginBottom: 10,
+    }
+});
